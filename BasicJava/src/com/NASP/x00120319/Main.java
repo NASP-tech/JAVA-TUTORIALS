904cdb5c2c9a30d89d444257ff264071d3ebcda9
Natalia Solorzano <00120319@uca.edu.sv>
2020-04-04 20:06:27 -0600

    Basic Java
    Basic Java

---
+++ BasicJava/src/com/NASP/x00120319/Main.java
@@ -0,0 +1,32 @@
+package com.NASP.x00120319;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("You can write whatever you want!");
+        System.out.println("A");
+        System.out.println("B");
+        //This is a comment!
+        //This disables a message
+        //Using Strings:
+
+        System.out.println("Madlibs!");
+        String name = "magicStar420!";
+
+        System.out.println("My _______ brother accidentally _________ed all over his new _________ ");
+
+        String adjective = "amazing";
+        String verb = "fart";
+        String noun = "Lamborgini car";
+
+        System.out.println("My " + adjective + " brother accidentally " + verb + "ed all over his new " + noun + ".");
+
+        //Using diferent spaces
+        System.out.println("Cat\nDod\nMonkey");
+        System.out.println("Cat\n\tDog\n\t\tMonkey");
+        
+
+
+    }
+}
